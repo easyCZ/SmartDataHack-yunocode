c781c9e6d7df53d86ebcb55d2bb6d7d51eed0e4e
Milan Pavlik <pavlik.mil@gmail.com>
2013-02-20 15:07:23 +0000

    DataObject added

---
+++ src/com/example/edw/DataObject.java
@@ -0,0 +1,13 @@
+package com.example.edw;
+
+
+public abstract class DataObject {
+		
+	abstract void setName(String name);
+	abstract String getName();
+	abstract void setLongtitude(double lng);
+	abstract double getLongtitude();
+	abstract void setLatitude(double lat);
+	abstract double getLatitude();
+
+}
